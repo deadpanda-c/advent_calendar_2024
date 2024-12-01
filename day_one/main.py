6de4e85ab8703c9179d85c20b1d56029904bee41
Clément Lagier <clement.lagier@epitech.eu>
2024-12-01 11:34:42 +0400

    [ADD] Completed the second part of the calendar

--- day_one/main.py
+++ day_one/main.py
@@ -11,21 +11,43 @@ class File:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.file.close()
-    
-    def run(self):
+
+    def get_side(self, file):
+        right_side = []
+        left_side = []
+        for line in file:
+            a, b = line.split()
+            if a.isdigit() and b.isdigit():
+                a = int(a)
+                b = int(b)
+                left_side.append(a)
+                right_side.append(b)
+            else:
+                print('Invalid input')
+        return left_side, right_side
+
+    def part_two(self):
+        score = 0
+        number_of_times = 0
+        right_side = []
+        left_side = []
+
+        with open(self.file_name, self.mode) as file:
+            left_side, right_side = self.get_side(file)
+        for i in range(len(left_side)):
+            for j in range(len(right_side)):
+                if left_side[i] == right_side[j]:
+                    number_of_times += 1
+            score += left_side[i] * number_of_times
+            number_of_times = 0
+        print(score)
+
+    def part_one(self):
         distance_array = []
         right_side = []
         left_side = []
-        with open('input.txt', 'r') as file:
-            for line in file:
-                a, b = line.split()
-                if a.isdigit() and b.isdigit():
-                    a = int(a)
-                    b = int(b)
-                    left_side.append(a)
-                    right_side.append(b)
-                else:
-                    print('Invalid input')
+        with open(self.file_name, self.mode) as file:
+            left_side, right_side = self.get_side(file)
         left_side.sort()
         right_side.sort()
         for i in range(len(left_side)):
@@ -36,4 +58,5 @@ class File:
         return abs(a - b)
 
 if __name__ == '__main__':
-    File('input.txt', 'r').run()
+    File('input.txt', 'r').part_one()
+    File('input.txt', 'r').part_two()
